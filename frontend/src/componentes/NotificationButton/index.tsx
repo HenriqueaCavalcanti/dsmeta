b71d077d4372094adaf5c78481fecfdc7c309116
Henrique Alves <henriquealves_cavalcanti@hotmail.com>
2022-07-17 21:15:28 -0300

    Send notification

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import icon from '../../recursos/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/resquest';
 import'./styles.css';
 
-function NotificationButton() {
+type props = {
+    saleId: number;
+}
+
+function handleClick(id :number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(Response => {
+        console.log("sucesso");
+    });
+}
+
+function NotificationButton( {saleId} : props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon}alt="Notificar" />
         </div>
     )
